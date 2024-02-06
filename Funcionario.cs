de810b1d7d582a86df19b587d5dfbd5155bfa44e
BrunooSilva13 <99100922+BrunooSilva13@users.noreply.github.com>
2024-02-06 20:37:53 -0300

    Iniciando classes

---
+++ Funcionario.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Funcionario
+    {
+        public string Nome;
+
+        public double Salario;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System;
+using System.Runtime.InteropServices;
+using System.Globalization;
+using System.Net.Http.Headers;
+
+namespace Program {
+class Program {
+static void Main(string[] args) {
+
+    Funcionario f1 = new Funcionario();
+    Funcionario f2 = new Funcionario();
+
+    System.Console.WriteLine("Dados do primeiro Funcionario: ");
+    System.Console.WriteLine("nome: ");
+    f1.Nome = Console.ReadLine();
+    Console.WriteLine("Salario: ");
+    f1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+    System.Console.WriteLine("Dados do segundo Funcionario: ");
+    System.Console.WriteLine("nome: ");
+    f2.Nome = Console.ReadLine();
+    System.Console.WriteLine("salario: ");
+    f2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    double media = (f1.Salario + f2.Salario) / 2.0;
+
+    Console.WriteLine("Salário médio = " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+        }   
+    }
+}
